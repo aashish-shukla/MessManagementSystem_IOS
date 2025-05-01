226cbbda354e23e98ebd47f678fed8fc5ba5562f
Aashish-shukla <118830317+aashish-shukla@users.noreply.github.com>
2025-05-01 15:47:17 +0530

    Delete MessManagementSystem directory

--- MessManagementSystem/MessManagementSystem/GlobalData.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-
-// Global array to store all users signup data
-var usersData: [[String: String]] = []
-
-// Variable to store current signed-in user
-var currentUser: [String: Any]? = nil
